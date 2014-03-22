8d446780d669749e050755cab45558ca8218bbab
Alimatou Keita <keitaalm@aol.com>
2014-03-22 18:13:00 -0400

    changed the name to graphite_packages

--- manifests/graphite_packages.pp
+++ manifests/graphite_packages.pp
@@ -1,4 +1,4 @@
-class graphite::pack-graphite {
+class graphite::graphite-packages {
 
      $packagesToBeInstalled = ["python-django-tagging", "python-twisted", "fontconfig", "python-simplejson", "python-devel.x86_64", "mod_wsgi.x86_64", "mod_python.x86_64", "python-sqlite2.x86_64", "bitmap.x86_64", "pycairo.x86_64", "python-zope-interface.x86_64", "python-simplejson.x86_64", "python-pip.noarch", "python-twisted-web.x86_64", "bitmap", "gcc-c++.x86_64", "make.x86_64", 
 
